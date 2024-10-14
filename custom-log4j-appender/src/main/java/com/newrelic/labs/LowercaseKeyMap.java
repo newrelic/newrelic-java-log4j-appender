384774ce4155bd87f08bbebd73a0f3cd99728916
gsidhwani_nr <gsidhwani@newrelic.com>
2024-10-14 13:30:13 +0530

    feat: first functional draft log4jAppender

---
+++ custom-log4j-appender/src/main/java/com/newrelic/labs/LowercaseKeyMap.java
@@ -0,0 +1,19 @@
+package com.newrelic.labs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class LowercaseKeyMap extends HashMap<String, Object> {
+	@Override
+	public Object put(String key, Object value) {
+		return super.put(key.toLowerCase(), value);
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		for (Map.Entry<? extends String, ? extends Object> entry : m.entrySet()) {
+			this.put(entry.getKey().toLowerCase(), entry.getValue());
+		}
+	}
+}
