c3d704e545401c413dbd126477131cff42e39f44
gsidhwani_nr <gsidhwani@newrelic.com>
2024-11-28 16:22:53 +0530

    fix: reque logentries failed due to case sensitive applicationName parameter

--- custom-log4j2-appender/src/main/java/com/newrelic/labs/LogEntry.java
+++ custom-log4j2-appender/src/main/java/com/newrelic/labs/LogEntry.java
@@ -2,13 +2,17 @@ package com.newrelic.labs;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LogEntry {
 	private final String message;
 	private final String applicationName;
 	private final String name;
 	private final String logtype;
 	private final long timestamp;
-
 
 	public LogEntry(String message, String applicationName, String name, String logtype, long timestamp,
 			Map<String, Object> custom, boolean mergeCustomFields) {
@@ -18,6 +22,26 @@ public class LogEntry {
 		this.logtype = logtype;
 		this.timestamp = timestamp;
 
+	}
+
+	// Default constructor for Jackson
+	public LogEntry() {
+		this.message = null;
+		this.applicationName = null;
+		this.name = null;
+		this.logtype = null;
+		this.timestamp = 0L;
+	}
+
+	@JsonCreator
+	public LogEntry(@JsonProperty("message") String message, @JsonProperty("applicationname") String applicationName,
+			@JsonProperty("name") String name, @JsonProperty("logtype") String logtype,
+			@JsonProperty("timestamp") long timestamp) {
+		this.message = message;
+		this.applicationName = applicationName;
+		this.name = name;
+		this.logtype = logtype;
+		this.timestamp = timestamp;
 	}
 
 	public String getMessage() {
